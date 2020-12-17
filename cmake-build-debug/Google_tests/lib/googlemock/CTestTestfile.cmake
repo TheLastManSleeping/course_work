cb87141eecc6fc59f3fdf61fcdeba455395d542c
arter <arthur.khalipov@gmail.com>
2020-12-17 17:38:26 +0300

    code clean up

--- cmake-build-debug/Google_tests/lib/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/c/ABC/Google_tests/lib/googlemock
-# Build directory: /cygdrive/c/ABC/cmake-build-debug/Google_tests/lib/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
